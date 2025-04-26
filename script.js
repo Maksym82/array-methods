7e2974947fdb36730a112a135b3a807a8781c692
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-04-26 11:09:55 +0300

    Update script

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-'use strict';
+"use strict";
 
 // filter
 
-const names = ['Anna', 'Ehor', 'Maksym', 'Valentin', 'Olha'];
+const names = ["Anna", "Ehor", "Maksym", "Valentin", "Olha"];
 
-const shortNames = names.filter(function(name) {
-    return name.length < 5;
+const shortNames = names.filter(function (name) {
+  return name.length < 5;
 });
 
 console.log(shortNames);
 
-
 // map
 
-const answers = ['MAKsyM', 'OLha', 'anNA', 'EhOR'];
+const answers = ["MAKsyM", "OLha", "anNA", "EhOR"];
 
-const result = answers.map(item => item.toLowerCase());
+const result = answers.map((item) => item.toLowerCase());
 
 console.log(result);
 
-
 // every/some
 
-const array = [5, 'Hello', 'Hi', 'Golden'];
+const array = [5, "Hello", "Hi", "Golden"];
 
-console.log(array.some(item => typeof(item) === 'number'));
+console.log(array.some((item) => typeof item === "number"));
 
-console.log(array.every(item => typeof(item) === 'number'));
-
+console.log(array.every((item) => typeof item === "number"));
 
 // reduce
 
@@ -36,20 +33,21 @@ const arr = [1, 3, 5, 7, 12, 15, 6];
 const res = arr.reduce((sum, current) => sum + current);
 console.log(res);
 
-const arr2 = ['apple', 'orange', 'tomato', 'peach', 'chery'];
+const arr2 = ["apple", "orange", "tomato", "peach", "chery"];
 
 const res2 = arr.reduce((sum, current) => `${sum}, ${current}`);
 console.log(res2);
 
 const obj = {
-    ivan: 'persone',
-    ann: 'persone',
-    dog: 'animal',
-    cat: 'animal'
+  ivan: "persone",
+  ann: "persone",
+  dog: "animal",
+  cat: "animal",
 };
 
 const newArray = Object.entries(obj)
-.filter(item => item[1] === 'persone')
-.map(item => item[0]);
+  .filter((item) => item[1] === "persone")
+  .map((item) => item[0]);
 
 console.log(newArray);
+
